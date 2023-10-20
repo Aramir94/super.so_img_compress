9630c131c36d25cbf220ac7a29f5c51f49a5eb9b
Aramir94 <jeonghyunsuk777@gmail.com>
2023-10-20 09:58:46 +0900

    ui change side bar add

--- main.py
+++ main.py
@@ -12,18 +12,16 @@ def compress_and_convert_to_jpg(img, quality=85):
 
 st.title('Image Compressor & JPG Converter')
 
-uploaded_file = st.file_uploader("Choose an image...", type=['png', 'jpeg', 'jpg', 'bmp', 'tiff'])
+# 사이드바에 업로드 및 퀄리티 슬라이더 배치
+uploaded_file = st.sidebar.file_uploader("Choose an image...", type=['png', 'jpeg', 'jpg', 'bmp', 'tiff'])
+quality = st.sidebar.slider('Select JPG Quality', 10, 100, 85)
 
 if uploaded_file:
     original_size = len(uploaded_file.read())
-    st.write(f"Original Image Size: {original_size / 1024:.2f} KB")
-    
     # 파일 포인터를 다시 처음으로 되돌리기 위함
     uploaded_file.seek(0)
     img = Image.open(uploaded_file)
     
-    st.write(f"We recommend 60 ~ 85")
-    quality = st.slider('Select JPG Quality', 10, 100, 85)
     compressed_img_data = compress_and_convert_to_jpg(img, quality)
     compressed_size = len(compressed_img_data)
     
@@ -31,20 +29,22 @@ if uploaded_file:
     col1, col2 = st.columns(2)
     
     with col1:
-        st.image(img, caption="Uploaded Image.", use_column_width=True)
+        st.image(img, caption="Uploaded Image.", width=300)  # 이미지 크기를 조정
         st.write(f"Original Image Size: {original_size / 1024:.2f} KB")
         
     with col2:
-        st.image(compressed_img_data, caption="Compressed Image.", use_column_width=True)
+        st.image(compressed_img_data, caption="Compressed Image.", width=300)  # 이미지 크기를 조정
         st.write(f"Compressed Image Size: {compressed_size / 1024:.2f} KB")
     
-    st.download_button("Download Compressed JPG", compressed_img_data, "compressed.jpg", "image/jpeg")
+    # 사이드바에 다운로드 버튼 배치
+    st.sidebar.download_button("Download Compressed JPG", compressed_img_data, "compressed.jpg", "image/jpeg")
 
     # 로드 시간 예상치 계산 (예: 5MB/s 속도로 다운로드할 경우)
     download_speed = 5 * 1024 * 1024  # 5 MB/s in bytes
     estimated_time_original = original_size / download_speed
     estimated_time_compressed = compressed_size / download_speed
-
+    
     st.write(f"Estimated load time (at 5MB/s):")
     st.write(f"Original Image: {estimated_time_original:.2f} seconds")
     st.write(f"Compressed Image: {estimated_time_compressed:.2f} seconds")
+
